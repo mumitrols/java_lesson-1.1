60d89e2974238c79d0de3269f7d777faa16567e6
mumitrols <mumitrols@bk.ru>
2022-01-07 15:07:51 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transferSum = 500_000_000;
+        int total = balance + transferSum;
+        System.out.println(total);
+    }
+}
